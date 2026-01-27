291dd063c10f26f6a040698e1d35cc1bee049288
felipe <felipe.garciar@upb.edu.co>
2026-01-27 07:37:32 -0500

    Auth

--- client/lib/auth-client.ts
+++ client/lib/auth-client.ts
@@ -1,6 +1,10 @@
 import {createAuthClient} from "better-auth/react";
+import {deviceAuthorizationClient} from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
     redirectTo: "/",
     baseURL: "http://localhost:3005",
+    plugins: [
+        deviceAuthorizationClient()
+    ]
 })
